d051f9703289f6dd508ed2435d083f7ed5cc3e16
Sauvik Chatterjee <73000146+sauvikchatterjee07@users.noreply.github.com>
2022-02-16 22:01:04 +0530

    Create 215. Kth Largest Element in an Array.cpp

---
+++ 215. Kth Largest Element in an Array.cpp
@@ -0,0 +1,7 @@
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int> pq(nums.begin(), nums.end());
+        while(--k)
+            pq.pop();
+        return pq.top();
+        
+    }
